45c7c426ae7c8304ea9cb8722774161307f1ad55
Tiago Valverde <tiago.c.v17@gmail.com>
2019-03-04 22:02:30 -0500

    File struct setup, db connection

---
+++ templates/inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <p>© 2018 Job Lister, Inc.</p>
+</footer>
+</div>
+</body>
+
+</html>
---
+++ templates/inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Job Lister</title>
+    <link rel="stylesheet" href="css/bootstrap.3.3.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header clearfix">
+            <nav>
+                <ul class="nav nav-pills pull-right">
+                    <li role="presentation" class="active"><a href="index.php">Home</a></li>
+                    <li role="presentation"><a href="create.php">Create Listing</a></li>
+                </ul>
+            </nav>
+            <h3 class="text-muted">
+                <?php echo SITLE_TITLE ?>
+            </h3>
+        </div>
